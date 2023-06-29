98912a88742cb4737fb5a648a29947ce647a2398
Tiago Papile <tpapile@studiovisual.com.br>
2023-06-29 18:52:28 -0300

    roe-425 Cria eventos de código pix copiado

--- src/assets/js/pix.js
+++ src/assets/js/pix.js
@@ -4,11 +4,13 @@ class vindiPix extends HTMLElement {
     // establish prototype chain
     super();
 
-    this.dialog       = document.getElementById('vindi-pix-dialog');
-    this.image        = this.dialog?.querySelector('.vindi-pix-dialog__image');
-    this.input        = this.dialog?.querySelector('.vindi-pix-dialog__input');
-    this.button       = this.dialog?.querySelector('.vindi-pix-dialog__button');
-    this.eventSuccess = new CustomEvent('success');
+    this.dialog           = document.getElementById('vindi-pix-dialog');
+    this.image            = this.dialog?.querySelector('.vindi-pix-dialog__image');
+    this.input            = this.dialog?.querySelector('.vindi-pix-dialog__input');
+    this.button           = this.dialog?.querySelector('.vindi-pix-dialog__button');
+    this.eventSuccess     = new CustomEvent('success');
+    this.eventCopySuccess = new CustomEvent('copysuccess');
+    this.eventCopyError   = new CustomEvent('copyerror');
 
     this.button?.addEventListener('click', (event) => this.copy(event));
   }
@@ -33,9 +35,13 @@ class vindiPix extends HTMLElement {
 
     try {
         await navigator.clipboard.writeText(this.input.value);
+
+        this.dispatchEvent(this.eventCopySuccess);
     }
     catch(error) {
-        console.log('copy error', error);
+        console.error('copy error', error);
+
+        this.dispatchEvent(this.eventCopyError);
     }
   }
 
